5f168436555f3393071c77b90e3a7109a5daade4
Larry Schultheis <larryschultheis@Larrys-MacBook-Pro.local>
2023-05-31 20:04:53 -0400

    Added basic auth0 integration via login/logout buttons.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
+import LoginButton from './LoginButton';
 import './App.css';
+import LogoutButton from './LogoutButton';
+import Profile from './Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
-function App() {
+
+function App() {    
   return (
     <div className="App">
       <header className="App-header">
+      <LoginButton/>
+      <LogoutButton/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
