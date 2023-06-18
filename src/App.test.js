4e8e7d3b79ab81335f2f784daa1130ca9494c1a1
Larry Schultheis <larryschultheis@Larrys-MacBook-Pro.local>
2023-06-18 14:14:50 -0400

    Added super basic test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginElement = screen.getByText(/Log In/i);
+  const signupElement = screen.getByText(/Sign Up/i);
+  expect(loginElement).toBeInTheDocument();
+  expect(signupElement).toBeInTheDocument();
+
 });
