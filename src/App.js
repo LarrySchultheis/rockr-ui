fb126239de407d3e697eb49cf8cc57cf6fe031c3
Shannon Quaill <shannonquaill@gmail.com>
2023-06-18 15:45:34 -0400

    update landing page and setup theme and routing framework

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import './App.css';
-import Profile from './components/Profile';
-import { useAuth0 } from '@auth0/auth0-react';
-import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import Bands from "./components/Bands";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import LandingPage from './pages/LandingPage';
+import AppFooter from './components/AppFooter';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './components/Theme';
 
-function App() {  
-  const { isAuthenticated } = useAuth0();
 
+const App = () => {
   return (
-    <div>
-      <Navbar></Navbar>
-      <Profile></Profile>
-    </div>
-
+    <ThemeProvider theme={theme}>
+      <head><link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      </head>
+      <Router>
+        <div>
+          <ResponsiveAppBar></ResponsiveAppBar>
+          <Routes>
+            <Route path="/bands" element={<Bands />}/>
+            <Route path="/" element={<LandingPage />}/>
+          </Routes>
+        </div>
+      </Router>
+      <AppFooter />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
