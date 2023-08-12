72d439cfadc8df2a80dfe427270e4c6488342c68
Larry Schultheis <schultheis@sawatchlabs.com>
2023-08-12 12:42:15 -0400

    boo

--- src/components/ChatService/ChatBody.js
+++ src/components/ChatService/ChatBody.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const ChatBody = (props) => {
-  const navigate = useNavigate();
   const {messages, user, currentMatch} = props;
 
   return (
